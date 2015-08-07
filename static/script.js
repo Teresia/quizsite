d2feb1ab96671bd6c39033d47264a536261b3a8d
Teresia <teresia.schullstrom@valtech.se>
2015-08-07 16:26:00 +0200

    DIVISION

--- static/script.js
+++ static/script.js
@@ -1,11 +1,17 @@
 $( document ).ready(function() {
     console.log( "readyyyyyy!" );
 
-
-
 $(".answerbox").click(function(){
 	$(".answerbox").removeClass("answerbox_checked");
 	$(this).addClass("answerbox_checked");
 	});
+
+// {% for quiz in quizzes %}
+// if ({{ correct }}/{{ total }} ) == 1{
+// 	$( "#result" ).replaceWith( "<h1>Alla rätt!</h1>" );
+// }
+// {% endfor %}
+
+
 
 });
